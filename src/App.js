f7470369457c636dbcc38e9243dc1fe7c127e539
eugene <eugnazaroff@gmail.com>
2021-01-08 00:52:26 +0500

    finished basic card structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import PokemonCard from "./components/pokemonCard/pokemonCard";
+import './App.css'
 
 const App = () => {
+
+    const testPoke = {
+        name: "Billy Eilish",
+        portraitUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png"
+    }
+
   return (
       <div>
-        
+        <PokemonCard pokemon={testPoke}/>
       </div>
   );
 };
