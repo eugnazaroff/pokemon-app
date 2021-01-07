6b5552a29709e507965e4e39c3033dd1ac008f6d
eugene <eugnazaroff@gmail.com>
2021-01-08 00:30:16 +0500

    initial commit

---
+++ src/components/pokemonCard/pokemonCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {useEffect} from "react/cjs/react.production.min";
+
+const PokemonCard = ({pokemon}) => {
+
+    return (
+        <div className="card">
+            
+        </div>
+    );
+};
+
+export default PokemonCard;
