a9a8ce448339a7d76b7c845b9ed7698cd2c4045e
eugene <eugnazaroff@gmail.com>
2021-01-08 01:13:58 +0500

    finished card

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
+
 import PokemonCard from "./components/pokemonCard/pokemonCard";
 import './App.css'
+import {useEffect, useState} from "react/cjs/react.production.min";
 
 const App = () => {
+
+
+    const [pokemons, setPokemons] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const pokemonData =
+        }
+
+
+    }, [])
 
     const testPoke = {
         name: "Billy Eilish",
         portraitUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png"
     }
 
-  return (
-      <div>
-        <PokemonCard pokemon={testPoke}/>
-      </div>
-  );
+    return (
+        <div>
+            <PokemonCard pokemon={testPoke}/>
+        </div>
+    );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import {Provider} from 'react-redux'
+import store from "./redux/store";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+
+    </React.StrictMode>,
+    document.getElementById('root')
 );
