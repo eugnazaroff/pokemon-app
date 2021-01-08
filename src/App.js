cd2e2d298399ca2090b80888080438dee42dd8ab
eugene <eugnazaroff@gmail.com>
2021-01-08 10:58:25 +0500

    created basic header

--- src/App.js
+++ src/App.js
@@ -2,26 +2,31 @@ import React, {useEffect, useState} from 'react';
 import './App.css'
 import axios from "axios";
 import PokemonCardList from "./components/pokemonCardList/pokemonCardList";
+import Header from "./components/header/header";
 
 const App = () => {
 
 
     const [pokemons, setPokemons] = useState({})
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         const fetchData = async () => {
             const pokemonData = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=21")
             setPokemons(pokemonData.data.results)
+            setLoading(false)
         }
         fetchData()
+
 
     }, [])
 
 
     return (
-        <div>
+        <div className="App">
+            <Header/>
             {
-                pokemons.length ?
+                !loading ?
                     <PokemonCardList pokemons={pokemons}/>
                     :
                     <p>Loading...</p>
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './header.scss'
+
 const Header = () => {
     return (
-        <div>
-            
+        <div className="header">
+            <div className="header__logo">
+                <h1 className="logo-text">
+                    <a href="/">Pokemon App</a></h1>
+            </div>
+            <nav className="header__menu">
+                <ul className="menu-list">
+                    <li className="menu-list__item"><a href="#">Home</a></li>
+                    <li className="menu-list__item"><a href="#">Pokemons</a></li>
+                </ul>
+            </nav>
         </div>
     );
 };
